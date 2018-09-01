c36c5e561cfe57ff622408bd262cec9f4b5e2eff
pc_zj <2639633457@qq.com>
2018-09-01 16:12:57 +0800

    init

---
+++ src/main/java/com/nxpay/online/OnlineApplication.java
@@ -0,0 +1,12 @@
+package com.nxpay.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineApplication.class, args);
+    }
+}
